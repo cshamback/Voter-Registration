3742fe3a8cc3c1155abf39a05605828a789ff380
cshamback <149506027+cshamback@users.noreply.github.com>
2023-11-16 09:13:09 -0600

    Create ValidateForm.h
    Validates the user's application form using algorithm chosen by strategy pattern.

---
+++ ValidateForm.h
@@ -0,0 +1,24 @@
+#ifndef VALIDATE_FORM_H
+#define VALIDATE_FORM_H
+
+/*
+Has two child classes, ValidateOnlineForm and ValidateOverseasForm.
+These two classes share some attributes and methods. They are the strategies
+used to validate the form.
+  */
+
+#import <string>
+
+using namespace std;
+
+class ValidateForm {
+public:
+  ValidateForm(); // default constructor
+
+  string registrationInfo[6]; // fName, lName, age, residency, ssn, felony
+  string formType;            // same as registrationInfo[3]
+
+  bool checkForm(); // also acts as a getter method
+};
+
+#endif
