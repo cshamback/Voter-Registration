073849740f8257154fae29156bf1c2b4ebd716fc
cshamback <149506027+cshamback@users.noreply.github.com>
2023-11-25 11:25:03 -0600

    Create ValidationStrategy.h
    Header file for ValidationStrategy class. Was not added along with .cpp file; added now. Changed return type of methods from void to bool so that main.cpp can use the results of the concrete strategies.

---
+++ ValidationStrategy.h
@@ -0,0 +1,18 @@
+// ValidationStrategy.h
+#ifndef VALIDATION_STRATEGY_H
+#define VALIDATION_STRATEGY_H
+
+#include "ValidateOnlineForm.h"
+#include "ValidateOverseasForm.h"
+#include <string>
+
+// base class for validation strategies
+class ValidationStrategy {
+public:
+  ValidationStrategy();
+
+  bool chooseOnlineStrategy(ValidateOnlineForm *form);
+  bool chooseOverseasStrategy(ValidateOverseasForm *form);
+};
+
+#endif
