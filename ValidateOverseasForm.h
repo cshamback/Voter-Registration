c8b078366b2c3ee561ec87920374c777e247d124
cshamback <149506027+cshamback@users.noreply.github.com>
2023-11-25 11:22:11 -0600

    Create ValidateOverseasForm.h
    Header for ValidateOverseasForm class

---
+++ ValidateOverseasForm.h
@@ -0,0 +1,20 @@
+#ifndef VALIDATE_OVERSEAS_FORM_H
+#define VALIDATE_OVERSEAS_FORM_H
+
+#include "ValidateForm.h"
+
+using namespace std;
+
+class ValidateOverseasForm
+    : public ValidateForm { // extends parent class ValidateForm
+public:
+  // put attributes and method headers here:
+  ValidateOverseasForm(); // default constructor
+  bool checkMilitary();
+
+  string registrationInfo[6]; // fName, lName, age, residency, ssn, felony
+  bool formIsValid;
+  string overseas;
+};
+
+#endif
