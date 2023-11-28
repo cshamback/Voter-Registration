80c736fe1ca72dbc0e8fd666117a95d58297e456
cshamback <149506027+cshamback@users.noreply.github.com>
2023-11-28 16:53:55 -0600

    Update ValidateOnlineForm.h
    Removed example code that was commented out. Added string* parameter to checkResidency().

--- ValidateOnlineForm.h
+++ ValidateOnlineForm.h
@@ -10,15 +10,10 @@ class ValidateOnlineForm
 {
 public:
   ValidateOnlineForm(); // default constructor
-  // put attributes and method headers here:
-  // example of an attribute:
-  // int number;
-  // example of a method header:
-  // void ValidateForm(parameterType parameterName);
 
   string registrationInfo[6]; // fName, lName, age, residency, ssn, felony
   bool formIsValid;
-  bool checkResidency();
+  bool checkResidency(string *formInfo);
 };
 
 #endif
