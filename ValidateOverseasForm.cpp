d4a3605565dbaaf4b28dc50832f7330a4588a8fa
cshamback <149506027+cshamback@users.noreply.github.com>
2023-11-28 16:56:28 -0600

    Update ValidateOverseasForm.cpp
    Deleted example code that was no longer needed. 
    Added while loop to checkMilitary() to make sure correct input is entered.

--- ValidateOverseasForm.cpp
+++ ValidateOverseasForm.cpp
@@ -7,30 +7,27 @@
 
 using namespace std;
 
-// define methods from class diagram here
-// example:
-// returnType ClassName::methodName(parameters) { definition }
-
 ValidateOverseasForm::ValidateOverseasForm() { // default constructor
   formIsValid = false;
 }
 
 bool ValidateOverseasForm::checkMilitary() {
+  while(i <= 0){
   cout << "Are you overseas and eligible to vote? (Please answer Y/N) " << endl;
   cin >> overseas;
 
   if (overseas == "y" || overseas == "Y") {
+    i++;
     formIsValid = true;
-    return 1;
   } else if (overseas == "n" || overseas == "N") {
     formIsValid = false;
-    return 0;
+    cout << "Ineligible to vote." << endl;
+    return formIsValid;
   } else {
     cout << "Invalid answer. Please answer Y/N " << endl;
-    return 0;
   }
-
-  return false;
+ } 
+  return formIsValid;
 }
 
 #endif
